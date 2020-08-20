ae74e644b53ce020d90716d651a782700ccb8bd6
uday <udaykiranravva123@gmail.com>
2020-08-20 20:45:48 +0530

    Updates

---
+++ precache-manifest.8677141be3042f59ea953a8976f801ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e645566f9c80ca0de081543b23c0582",
+    "url": "/todo-demo/index.html"
+  },
+  {
+    "revision": "5eba91e093c546c63377",
+    "url": "/todo-demo/static/css/main.38f7b17a.chunk.css"
+  },
+  {
+    "revision": "cae303a678086002c6b6",
+    "url": "/todo-demo/static/js/2.4109f84e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-demo/static/js/2.4109f84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eba91e093c546c63377",
+    "url": "/todo-demo/static/js/main.c6844356.chunk.js"
+  },
+  {
+    "revision": "23d7c0f146e11201e14c",
+    "url": "/todo-demo/static/js/runtime-main.e871745e.js"
+  }
+]);
